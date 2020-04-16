9dda41523df64414a3736303eacb7819ec4a88a3
Daniel Windsor <dw.accounts@protonmail.com>
2020-04-17 11:47:13 +1200

    Adjust route

--- server/server.js
+++ server/server.js
@@ -7,6 +7,6 @@ const randTaco = require('./routes/savedTaco')
 server.use(express.json())
 server.use(express.static('public'))
 
-server.use('/api/v1/savedTacos', randTaco)
+server.use('/v1/savedTacos', randTaco)
 
 module.exports = server
