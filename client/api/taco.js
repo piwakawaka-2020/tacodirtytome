fae9256ee218331c97b4e9bdaf0b19a0475bc56c
tcodling <thomas.codling@outlook.com>
2020-04-17 10:20:09 +1200

    add taco routes

--- client/api/taco.js
+++ client/api/taco.js
@@ -5,3 +5,9 @@ export function getRandomTaco() {
         .get('http://taco-randomizer.herokuapp.com/random/')
         .then(res => res.body)
 }
+
+export function getRandomFullTaco() {
+    return request
+        .get('http://taco-randomizer.herokuapp.com/random/?full-taco=true')
+        .then(res => res.body)
+}
