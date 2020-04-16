9af860cfcb68ea6eb8239016a5f7d65ffd99b3eb
Josh Wood <JHW300@gmail.com>
2020-04-17 11:25:21 +1200

    Internal Api Functions

---
+++ client/api/internalTaco.js
@@ -0,0 +1,67 @@
+// REST
+// # Create:
+// Http Verb: POST
+// Path: http://taco-randomizer.herokuapp.com/random/
+
+
+// # Read
+// Http Verb: GET
+// Path: http://taco-randomizer.herokuapp.com/random/  (Collection of random tacos)
+// {
+//     "mixin": {
+//         "name": "Traditional Taco Mixins",
+//         "recipe": "Traditional Taco Mixins\n(makes about 20 servings)\n\n- 1 (16 ounce) container sour cream\n- 1/4 head iceberg lettuce (rinsed, dried, shredded)\n- 1 cup shredded cheddar cheese\n- 3 diced tomatoes\n- 1 (2.25 ounce) can black olives, drained\n\nAdd ingredients to tacos in layers. \n\ntags: vegetarian\n\n",
+//         "slug": "traditional_taco_mixins",
+//         "url": "https://raw.github.com/sinker/tacofancy/master/mixins/traditional_taco_mixins.md"
+//     },
+//     "base_layer": {
+//         "name": "Basic Shredded Chicken",
+//         "recipe": "Basic Shredded Chicken\n======================\n\nThis recipe creates enough shredded chicken for 2 to 3 meals. It's a savory, universal base; once cooked, you can supplement with any number of spices or treatments to make tacos, taquitos, soups, enchiladas ... even Italian or Indian applications if you like.\n\n__In the Crockpot__\n\n* 4-5 lbs. boneless skinless chicken breast\n* 1 can chicken broth\n* 1 tsp chili powder\n* 1/2 tsp cumin\n* 1/2 tsp garlic powder\n* 1/2 tsp oregano\n* kosher salt\n* black pepper\n\nSalt the chicken breasts (be reasonably generous here), then crack some black pepper over each. In your crockpot, mix garlic powder, chili powder, cumin and oregano into the broth. Place chicken in crockpot, then cook on low for 8 hours.\n\nWhen the chicken is done, attack each piece with two forks to shred. Do this on a Sunday and you'll have an easy time putting together meals throughout the week.\n\nI often give this a quick simmer with a bit of water and [universal taco seasoning](/seasonings/universal_taco_seasoning.md) for taco filling. Or throw it into tortilla soup. Or make some sweet [taquitos](/like_tacos/baked_taquitos.md). Or just add some barbecue sauce to a handful of chicken, and toss it onto a soft roll for a quick sandwich. Man *can* live on tacos alone, but one doesn't have to.",
+//         "slug": "basic_shredded_chicken",
+//         "url": "https://raw.github.com/sinker/tacofancy/master/base_layers/basic_shredded_chicken.md"
+//     },
+//     "condiment": {
+//         "name": "Salsa de chile de árbol",
+//         "recipe": "Salsa de chile de árbol\n=======================\n\nThis is a go-to taco sauce in western Mexico and probably elsewhere. You can\nmix and match the techniques in the two variants presented here, and substituting\nor adding chiles is a ramp onto the combinatorial superhighway of Mexican food and\ncan demystify the salsa bar at the local taquería.\n\n# Red\n\n* dried whole *chiles de árbol*\n* tomatoes\n* onion\n* *optionally*, garlic\n\nTake between .3 and 1 chiles per tomato, remove the stems and put them in a\nsaucepan with a tablespoon or two of oil (olive, soy, etc.) The pan and oil should be very\nhot and the chiles should seem to be at risk of burning. Rotate the\nchiles around and let them get dark brown to black without actually burning them —\nthis can happen quickly, which is good because your eyes, nose, and lungs\nmay reach their maximum vaporized chile tolerance during this step.\n\nAdd a bunch of chopped onion and garlic and lower the heat. Cut the tomatoes\nin half and throw them in with salt to taste. Cook until the tomatoes look stewed.\nIf you have one of those blender wands that you can put directly in a saucepan,\nyou can blend everything now (you may end up making this sauce frequently enough\nto justify the purchase of a blender wand.) Otherwise wait until the mixture\nis not hot and blend it in a standard blender.\n\n# Green\n\n* dried whole *chiles de árbol*\n* green tomatillos\n* onion\n* cilantro\n\nTake between .1 and .5 chiles per tomatillo, remove the stems and put them in a bare, hot saucepan.\nToast the chiles without oil until they are dark. Add halved or quartered tomatillos, salt, and a\ntablespoon or two of water to prevent burning the chiles before the tomatillos release\ntheir juices. When the mixture has cooked for as long as you can stand to wait, remove\nthe heat and let it cool. Blend and place in a little salsa bowl and heap with finely\nchopped onion and cilantro.\n\nOne non-obvious little point is that skipping the oil in the chile roasting-toasting\nstep seems to increase the spiciness of the final product.\n\ntags: vegetarian\n",
+//         "slug": "salsa_de_chile_de_arbol",
+//         "url": "https://raw.github.com/sinker/tacofancy/master/condiments/chile_de_arbol_sauce.md"
+//     },
+//     "shell": {
+//         "name": "Hard Corn Shells (Traditional; US)",
+//         "recipe": "Hard Corn Shells (Traditional; US)\n======================\n\nMistakenly thought by many to be traditionally Mexican, hard shells were actually popularized in the US in the mid-20th century.\n\nWhile they can certainly be made at home (if you have access to a deep-fryer), the best method of obtaining hard taco shells is to head to the grocery store.\n\nIf you line them with a lettuce leaf rather than using chopped lettuce, when the shell cracks you won't lose the contents into your lap.\n\ntags: vegetarian\n",
+//         "slug": "hard_corn_shells_traditional_us",
+//         "url": "https://raw.github.com/sinker/tacofancy/master/shells/hard_corn_traditional_us.md"
+//     },
+//     "seasoning": {
+//         "name": "Universal Taco Seasoning",
+//         "recipe": "Universal Taco Seasoning\n========================\n\nI got tired of buying packets of store-bought taco seasoning, so I experimented with various spices and ratios until I landed on this recipe. I keep a jar of it in the cupboard at all times.\n\n* 6 tbsp chili powder\n* 4 tbsp cumin\n* 4 tbsp corn starch\n* 3 tbsp onion powder\n* 1 tbsp salt\n* 1 tbsp garlic powder\n* 4 tsp oregano (Mexican oregano, if you've got it)\n* 2 tsp crushed red pepper\n\nCombine in Mason jar and shake well to combine.\n\nThis mix works well for chicken, pork and beef, destined for the grill, oven, slow cooker or stovetop. You could tweak it a bit to target a specific meat, but I like to have a base, universal mix around. Makes it super easy to turn leftover anything into delicious taco filling: Just chop up whatever it is, toss it into a skillet, sprinkle generously with seasoning, then add a bit of water and simmer to impart flavor.\n\ntags: vegetarian, vegan\n",
+//         "slug": "universal_taco_seasoning",
+//         "url": "https://raw.github.com/sinker/tacofancy/master/seasonings/universal_taco_seasoning.md"
+//     }
+// }
+
+import request from 'superagent'
+
+export function getAllTacos() {
+    return request.get('v1/taco')
+        .then(req => {
+            return req.body
+        })
+}
+
+export function addTaco(taco) {
+    return request.post('/v1/taco')
+    .post(taco)
+    .then(req => {
+        return req.body
+    })
+}
+
+export function getTacoById(id) {
+    return request.get('/v1/taco/' + id)
+    .then(req => {
+        return req.body
+    })
+}
+
+
