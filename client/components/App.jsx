c28c828a1433b9eeb98eb5e03ce7ac0210805f60
Pat Gonzales <patvalentinegonzales@gmail.com>
2020-04-17 12:16:47 +1200

    add get random taco button

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,50 @@
 import React from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 
 import TacoSearch from './TacoSearch'
 import TacoList from './TacoList'
 import TacoDetail from './TacoDetail'
 
-import { getRandomTaco } from '../api/taco'
+import { getRandomTaco, getRandomFullTaco } from '../api/taco'
 
 class App extends React.Component {
+  state = {
+    condiment: '',
+    mixin: '',
+    base_layer: '',
+    shell: '',
+    seasoning: ''
+  }
+
+  onClickHandler = event => {
+    getRandomTaco()
+    .then((taco) => {
+      console.log(taco)
+      this.setState({
+        condiment: taco.condiment,
+        mixin: taco.mixin,
+        base_layer: taco.base_layer,
+        shell: taco.shell,
+        seasoning: taco.seasoning
+      })
+    })
+  }
 
   render(){
     return (
       <Router>
-        <h1>Tacodirtytome</h1>
+        <div className="block">
+          <nav className="navbar">
+            <div className="navbar-brand">
+              <Link to="" className="navbar-item has-background-primary">
+                <h1 className="title is-3 ">Tacodirtytome</h1>
+              </Link>
+            </div>
+          </nav>
+        </div>
+        <div>
+          <button className="button" onClick={this.onClickHandler.bind(this)}> Get Random Taco </button>
+        </div>
         <Route to='/' component={TacoSearch}/>
         <Route to='/list' component={TacoList}/>
         <Route to='/list/:id' component={TacoDetail}/>
